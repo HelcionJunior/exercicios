f97a4ca32b14c09738057112990f614cfb0d2722
HelcionJunior <suporteacampo@gmail.com>
2018-11-07 08:36:31 -0200

    variaveis1.php

---
+++ variaveis.php
@@ -0,0 +1,28 @@
+<?php
+
+$nome = "junior";
+$sobrenome = "silva";
+$endereco = "belacintra";
+$idade = "37";
+$telefone = "953672700";
+
+echo "Meu nome é ";
+echo $nome;
+echo "</br>";
+echo "Meu sobrenome é ";
+echo $sobrenome;
+echo "</br>";
+echo "Moro na ";
+echo $endereco;
+echo "</br>";
+echo "Minha idade é ";
+echo $idade;
+echo "</br>";
+echo "Meu número de telefone é ";
+echo $telefone;
+
+
+
+//Utilizando o que vimos na última aula, imprima, utilizando-se de variáveis, os 
+// dados de uma pessoa como:
+// Nome, sobrenome, endereço, idade e telefone.
