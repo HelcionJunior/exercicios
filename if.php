6ee0402c1e012cd26eb6744380c197a261ec6588
HelcionJunior <suporteacampo@gmail.com>
2018-11-07 11:01:42 -0200

    if1

---
+++ if.php
@@ -0,0 +1,17 @@
+<?php
+$nome = "Junior";
+$idade = 21;
+
+echo "olá " . $nome . " ,tudo bem?</br>";
+
+if ($idade >= 21) {
+    echo "Sim, você tem maior idade jurídica";
+} else {
+    echo "Que pena, você ainda é um bebê";
+}
+
+// A partir do exemplo mostrado na aula, faça um programa que informe se a pessoa tem ou 
+//não maioridade jurídica.
+// Utilizaremos como critério para dizer se ele tem ou não maioridade jurídica ser maior de 
+//21 anos.
+
