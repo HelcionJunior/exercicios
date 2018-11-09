6b87978c82bf73cb9b1876d00290ad573b995c1f
HelcionJunior <suporteacampo@gmail.com>
2018-11-09 02:58:44 -0200

    switch1

---
+++ switch.php
@@ -0,0 +1,29 @@
+<?php
+
+$opcao = 5;
+
+switch ($opcao) {
+    case '1':
+        echo "Lasanha";
+        break;
+    case '2':
+        echo "Macarrão";
+        break;
+    case '3':
+        echo "Nhoque";
+        break;
+    case '4':
+        echo "Contra-Filé";
+        break;
+    case '5':
+        echo "Peixe";
+        break;
+    case '6':
+        echo "Picadinho";
+        break;
+    
+}
+
+/*Utilizando a instrução SWITCH crie um cardápio com 6 opções de p
+rato que vão ser "selecioanados" (mostrado em seu programa) de acordo 
+com o número do item (1,2,3..)*/
