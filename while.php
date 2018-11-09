828e2cddd3d2de764f2f0767640264bee80a3e28
HelcionJunior <suporteacampo@gmail.com>
2018-11-09 00:55:20 -0200

    while1

---
+++ while.php
@@ -0,0 +1,14 @@
+<?php
+
+$contador = 5000;
+
+while ($contador <= 5543) {
+    echo $contador . "</br>";
+    $contador = $contador + 1;
+}
+
+
+
+
+//Crie um programa em PHP utilizando a estrutura de repetição
+// WHILE que seja capaz de contar de 5.000 a 5.543
