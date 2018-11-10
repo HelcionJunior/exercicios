87a683bc106d3eb1c0418adef79e781b8b03a70e
HelcionJunior <suporteacampo@gmail.com>
2018-11-10 20:06:42 -0200

    capitais

---
+++ capitais.php
@@ -0,0 +1,15 @@
+<?php
+$capitais = array("São Paulo ", "Rio de Janeiro ","Belo Horizonte "," Salvador",
+" Vitória"," Brasília"," Aracaju"," Recife"," Florianópolis",
+" Curitiba",);
+
+echo current($capitais)."<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
