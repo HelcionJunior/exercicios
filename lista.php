8be71d37b7d285846718de7e67f514f8bedde984
HelcionJunior <suporteacampo@gmail.com>
2018-11-10 13:13:25 -0200

    lista

---
+++ lista.php
@@ -0,0 +1,25 @@
+<?php
+
+$funcionarios = array("funcionario 1 ","funcionario 2 ","funcionario 3 ",
+"funcionario 4 ","funcionario 5 ","funcionario 6 ","funcionario 7 ",
+"funcionario 8 ","funcionario 9 ","funcionario 10 ",);
+
+echo $funcionarios [0];
+echo "</br>";
+echo $funcionarios [1];
+echo "</br>";
+echo $funcionarios [2];
+echo "</br>";
+echo $funcionarios [3];
+echo "</br>";
+echo $funcionarios [4];
+echo "</br>";
+echo $funcionarios [5];
+echo "</br>";
+echo $funcionarios [6];
+echo "</br>";
+echo $funcionarios [7];
+echo "</br>";
+echo $funcionarios [8];
+echo "</br>";
+echo $funcionarios [9];
