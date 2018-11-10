428d2fbc543b020b7cc84b07532cd03c8d449cb6
HelcionJunior <suporteacampo@gmail.com>
2018-11-10 18:13:22 -0200

    foreach

---
+++ foreach.php
@@ -0,0 +1,10 @@
+<?php
+
+$times = array("corinthians","palmeiras","internacional","flamengo","sao paulo",
+"gremio","atletico mineiro","santos","cruzeiro","atletico paranaense","fluminense",
+"bahia","vasco","botafogo","ceara","sport","america mineiro","vitoria",
+"chapecoense","parana",);
+
+foreach ($times as $totalTimes) {
+    echo $totalTimes . "</br>";
+}
