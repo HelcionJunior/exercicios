32434690f3e686e0793f24a57267295b385e4dcf
HelcionJunior <suporteacampo@gmail.com>
2018-11-11 01:29:50 -0200

    associativos

---
+++ associativos.php
@@ -0,0 +1,28 @@
+<?php
+
+$saopaulo = array(
+    "aniversário" => "25 de Janeiro",
+    "fundação"    => "Janeiro de 1554",
+    "gentílico"   => "paulistano",
+    "lema"        => "Não sou conduzido, conduzo",
+    "padroeiro"   => "Apóstolo Paulo",
+    "prefeito"    => "Bruno Covas",
+);
+
+    echo $saopaulo["aniversário"];
+    echo "<br>";
+    echo $saopaulo["fundação"];
+    echo "<br>";
+    echo $saopaulo["gentílico"];
+    echo "<br>";
+    echo $saopaulo["lema"];
+    echo "<br>";
+    echo $saopaulo["padroeiro"];
+    echo "<br>";
+    echo $saopaulo["prefeito"];
+    echo "<br>";
+
+
+/*Crie um array com informaçoes sobre a cidade de São Paulo, nele devem constar dados como:
+     Aniversário, fundação, gentílico, 
+lema, padroeiro e prefeito. */
