07359f92155891246d291427970858a31f3dd7ae
HelcionJunior <suporteacampo@gmail.com>
2018-11-11 02:54:43 -0200

    formalunos

---
+++ formalunos.php
@@ -0,0 +1,8 @@
+<form action= "formalunos2.php" method="GET">
+<label>Nome</label><br>
+<input type= "text" name="nome"/><br>
+<label>Turma</label><br>
+<input type= "text" name="turma"/><br>
+<input type= "submit" value= "Enviar"/><br>
+
+</form>
---
+++ formalunos2.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "aluno e turma enviados<br>";
+echo "aluno: " . $_GET["nome"]. "<br>";
+echo "turma: " . $_GET["turma"]. "<br>";
