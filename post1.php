3400353a5c9ae286ea93cb105d0968cfe7fb5c37
HelcionJunior <suporteacampo@gmail.com>
2018-11-11 03:13:50 -0200

    post

---
+++ post1.php
@@ -0,0 +1,8 @@
+<form action= "post2.php" method="POST">
+<label>Nome</label><br>
+<input type= "text" name="nome"/><br>
+<label>Turma</label><br>
+<input type= "text" name="turma"/><br>
+<input type= "submit" value= "Enviar"/><br>
+
+</form>
---
+++ post2.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "aluno e turma enviados<br>";
+echo "aluno: " . $_POST["nome"]. "<br>";
+echo "turma: " . $_POST["turma"]. "<br>";
+
+echo "<br><br>";
+var_dump($_POST);
