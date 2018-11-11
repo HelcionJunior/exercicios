228f2ae0cbf39a8914721c03bbf6828a60b38bb7
HelcionJunior <suporteacampo@gmail.com>
2018-11-11 04:21:45 -0200

    turno do aluno

---
+++ turno1.php
@@ -0,0 +1,10 @@
+<form action= "turno2.php" method="POST">
+<label><h3>Nome Completo</h3></label>
+<input type= "text" name="nome"/><br><br>
+<label><h3>Turno</h3></label>
+<input type= "radio" name="turno" value="manhã"/>Manhã<br>
+<input type= "radio" name="turno" value="tarde"/>Tarde<br>
+<input type= "radio" name="turno" value="noite"/>Noite<br><br>
+<input type= "submit" value= "Enviar"/><br>
+
+</form>
